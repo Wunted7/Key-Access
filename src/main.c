81c477cf956d1d45610dc4bb38441efcd39e42bf
wunted7 <wunted7@gmail.com>
2017-05-01 20:18:18 +0300

    Add bin file

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 #include <math.h>
 #include <allegro5/allegro_font.h>
 
-FILE *C, *X;
+FILE *C;
 double *A = NULL, s, m = 0.0;
 int i;
 int input()
@@ -26,10 +26,10 @@ int input()
 	double time_C = 0.0;
 	bool quit = false;
 
-	
-	char KEY_CODES[27] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','\0' };
-	int  m = 0, h = 0, c = 0;
-	char a, k;
+	//char KEY_CODES[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', };
+	//KEY_CODES[74] = ' ';
+
+	//
 	if (!al_init()) {
 		return -1;
 	}
@@ -45,7 +45,7 @@ int input()
 	}
 
 	ALLEGRO_FONT* font = al_create_builtin_font();
-	ALLEGRO_USTR* str = al_ustr_new("Pack my box with five dozen liquor jugs.");
+	ALLEGRO_USTR* str = al_ustr_new("pack my box with five dozen liquor jugs.");
 	ALLEGRO_USTR* tab = al_ustr_new("");
 	int pos = (int)al_ustr_size(str);
 	int pos1 = (int)al_ustr_size(tab);
@@ -56,6 +56,21 @@ int input()
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	//
+
+	C = fopen("input.bin", "rb");
+
+
+	while (fread(&N, sizeof(double), 1, C) > 0)//Берет информацию из файла и записывает в динамический массив
+	{
+		A = (double*)realloc(A, (i + 1) * sizeof(double));
+		A[i] = N;
+		s += A[i];
+		i++;
+	}
+	fclose(C);
+
+	C = fopen("input.bin", "wb");
+
 	while (!quit)
 	{
 		al_clear_to_color(al_map_rgb_f(0, 0, 0));
@@ -66,40 +81,14 @@ int input()
 
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
-		C = fopen("input.txt", "r");
-		X = fopen("input1.txt", "w");
-
-		m = h = ev.keyboard.keycode-1;
-		while (h != -1 && (fscanf(C, "%f", &N)) >= 0)
-		{
-			if (h == 0)
-			{
-				A = (double*)realloc(A, (i + 1) * sizeof(double));
-				A[i] = N;
-				s = s + A[i];
-				i++;
-			}
-			else
-			{
-				fprintf(X, " %f", N);
-			}
-			if (fgetc(C) == '\n')
-			{
-				h--;
-				if (h != 0 && m != 0)
-					fprintf(X, "\n");
-			}
-		}
-		if (m == 6)
-			fprintf(X, "\n");
 
 		if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			quit = true;
 		}
 		if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
 			if (ev.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-				al_ustr_append_cstr(tab, "\n");
-				//quit = true;
+				//al_ustr_append_cstr(tab, "\n");
+				quit = true;
 			}
 			time_A = al_get_time();
 		}
@@ -112,36 +101,6 @@ int input()
 			s += A[i];
 			++i;
 		}
-		for (j = 0; j<i; j++)
-			fprintf(X, " %f", A[j]);
-		fprintf(X, "\n");
-
-		while (fscanf(C, "%f", &N) >= 0)
-		{
-			fprintf(X, " %f", N);
-			if (fgetc(C) == '\n')
-			{
-				fprintf(X, "\n");
-			}
-
-		}
-		fclose(C);
-		fclose(X);
-		//fclose(X);
-		C = fopen("input.txt", "w");
-		X = fopen("input1.txt", "r");
-		// fseek(X,0,SEEK_SET);
-		while (!feof(X))
-		{
-			fscanf(X, "%f", &N);
-			fprintf(C, " %f", N);
-			if (fgetc(X) == '\n')
-			{
-				fprintf(C, "\n");
-			}
-		}
-		//fclose(X);
-		//remove("input1.txt");
 		if (ev.type == ALLEGRO_EVENT_KEY_CHAR) {
 			if (ev.keyboard.unichar >= 32)
 			{
@@ -156,6 +115,10 @@ int input()
 
 
 	}
+	for (j = 0; j < i; ++j) {
+		fwrite(&A[j], sizeof(double), 1, C);
+	}
+	fclose(C);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
 	return 0;
@@ -193,14 +156,6 @@ double Stewdent(int i)//Выводит коэфициент Стьюдента
 		return 2.09;
 	if (i <= 30 && i>20)
 		return 2.04;
-	if (i <= 40 && i>30)
-		return 2.02;
-	if (i <= 50 && i>40)
-		return 2.01;
-	if (i <= 60 && i>50)
-		return 2.00;
-	if (i > 60)
-		return 1.96;
 
 }
 
