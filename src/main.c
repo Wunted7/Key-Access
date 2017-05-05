18693d680789c3372a6c6ab0c9372a566bd3d4b6
YuryBatov <batov1998@list.ru>
2017-05-05 17:51:23 +0300

    Файловая сиситема

--- src/main.c
+++ src/main.c
@@ -11,8 +11,19 @@
 #include <allegro5/allegro_font.h>
 #include "input.h"
 
-int main(void) { //Считает погрешности с динамическим выделением памяти
-	input();
+int main(void) {//Считает погрешности с динамическим выделением памяти
+	setlocale(LC_ALL, "Rus");
+	char a;
+	printf("Здравсивуйте ,введите 1 если хотите инициализированься и 2 если хотите авторизоваться: ?");
+	scanf("%c", &a);
+	if (a == '1')
+	{
+		input();
+	}
+	//if (a == '2') \
+	{\
+		output();\
+	}
 	printf("OK");
 	return 0;
 
