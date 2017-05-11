415f4345d6c27532f507979edd1be98dd50737f3
Wunted7 <Wunted@yandex.ru>
2017-05-11 23:37:00 +0300

    Update input.h

--- src/input.h
+++ src/input.h
@@ -1,6 +1,15 @@
 #pragma once
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <locale.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <math.h>
+#include <allegro5/allegro_font.h>
 #define MAX_SIZE 50
 #define MAX_LETTER 26
+
 
 double A[MAX_LETTER][MAX_SIZE];
 
