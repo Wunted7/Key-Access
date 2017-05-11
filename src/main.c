04a965c5b99db95fccc52b1bf990c8afffa8cc75
Wunted7 <Wunted@yandex.ru>
2017-05-11 23:39:51 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <locale.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <malloc.h>
-#include <stdlib.h>
-#include <math.h>
-#include <allegro5/allegro_font.h>
 #include "input.h"
 
 int main(void) {//Считает погрешности с динамическим выделением памяти
