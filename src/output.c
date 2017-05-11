d45aa1d357390334912e4328458a6e4d9bef1a04
Wunted7 <Wunted@yandex.ru>
2017-05-11 23:42:16 +0300

    Update output.c

--- src/output.c
+++ src/output.c
@@ -1,19 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <locale.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <malloc.h>
-#include <stdlib.h>
-#include <math.h>
-#include <allegro5/allegro_font.h>
 #include "input.h"
 #define MAX_SIZE 50
-#define MAX_LATTER 26
+#define MAX_LETTER 26
 
 FILE *C,*NAME,*X;
-double A[MAX_LATTER][MAX_SIZE], B[MAX_LATTER], D[MAX_LATTER][2];
+double A[MAX_LETTER][MAX_SIZE], B[MAX_LETTER], D[MAX_LETTER][2];
 
 int output()
 {
@@ -71,7 +61,11 @@ int output()
 			time_A = al_get_time();
 		}
 		if (ev.type == ALLEGRO_EVENT_KEY_UP)
-		 {
+		{
+			if (ev.keyboard.keycode > 27) {
+				continue;
+			}
+			else {
 				time_B = al_get_time();
 				while (A[ev.keyboard.keycode - 1][k] != 0)
 				{
@@ -79,18 +73,19 @@ int output()
 				}
 				A[ev.keyboard.keycode - 1][k] = time_B - time_A;
 				sr_zn = time_B - time_A;
-				if(k!=0)
+				if (k != 0)
 				{
-				for (count = 0; count < k; count++)
-				{
-					sum = sum + A[ev.keyboard.keycode - 1][count];
-				}
+					for (count = 0; count < k; count++)
+					{
+						sum = sum + A[ev.keyboard.keycode - 1][count];
+					}
 					sr_zn = sum / k;
 				}
 				k = 0;
 				sum = 0;
 				B[ev.keyboard.keycode - 1] = sr_zn;
 
+			}
 		}
 		if (ev.type == ALLEGRO_EVENT_KEY_CHAR) {
 			if (ev.keyboard.unichar >= 32)
@@ -105,28 +100,21 @@ int output()
 		}
 	}
 	NAME = fopen("NAME", "r");
-		fscanf(NAME, "%s", &name);
-		strncat1(name);
-		C = fopen(name, "r");
-		for (count = 0; count < MAX_LATTER; ++count)
+	fscanf(NAME, "%s", &name);
+	strncat1(name);
+	C = fopen(name, "r");
+	for (count = 0; count < MAX_LATTER; ++count)
+	{
+		for (j = 0; j < 2; ++j)
 		{
-			for (j = 0; j < 2; ++j)
-			{
-				fscanf(C, "%lf", &D[count][j]);
-			}
-			if (B[count] < (D[count][0] + D[count][1]) && B[count] > (D[count][0] - D[count][1]))
-			{
-				 ++count_1;
-			}
+			fscanf(C, "%lf", &D[count][j]);
 		}
-		fclose(C);
-	X = fopen("toto.txt", "w");
-	for (j = 0; j<MAX_LATTER; j++)
+		if (B[count] < (D[count][0] + D[count][1]) && B[count] > (D[count][0] - D[count][1]))
 		{
-			fprintf(X, "%.8lf ", B[j]);
-			fprintf(X, "\n");
+			 ++count_1;
 		}
-	fclose(X);
+	}
+	fclose(C);
 	al_destroy_event_queue(event_queue);
 	al_destroy_display(display);
 	printf("%i ", count_1);
